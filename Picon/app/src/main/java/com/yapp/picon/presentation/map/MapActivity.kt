e4291994206de7f8ede4a947ede9df473e028f5b
NackUn <king4521@naver.com>
2020-10-12 18:16:13 +0900

    [Issue-8] Main 클래스명, xml명 수정 및 ViewModel 적용

--- Picon/app/src/main/java/com/yapp/picon/presentation/map/MapActivity.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/map/MapActivity.kt
@@ -1,13 +1,19 @@
 package com.yapp.picon.presentation.map
 
+import androidx.activity.viewModels
 import com.naver.maps.map.*
 import com.yapp.picon.R
+import com.yapp.picon.databinding.MapActivityBinding
 import com.yapp.picon.presentation.base.BaseMapActivity
 
-class MainActivity : BaseMapActivity(
-    R.layout.activity_main,
+class MapActivity : BaseMapActivity<MapActivityBinding, MapViewModel>
+    (
+    R.layout.map_activity,
     R.id.mapFrame
 ) {
+
+    override val vm: MapViewModel by viewModels()
+
     private lateinit var map: NaverMap
 
     override fun onMapReady(naverMap: NaverMap) {
