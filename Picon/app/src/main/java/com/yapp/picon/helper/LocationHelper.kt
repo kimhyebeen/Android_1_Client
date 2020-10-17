7a7aa02fef64c8b01e0ea35d54d85580978496ca
kimhyebeen <kimhbin@naver.com>
2020-10-17 16:56:45 +0900

    [Issue-11] 위치권한을 확인하고 요청하는 클래스 LocationHelper 추가

---
+++ Picon/app/src/main/java/com/yapp/picon/helper/LocationHelper.kt
@@ -0,0 +1,10 @@
+package com.yapp.picon.helper
+
+import android.content.Context
+import android.location.LocationManager
+
+class LocationHelper(context: Context) {
+    private var locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    private val LOCATION_REQUEST_CODE = 200
+
+}
