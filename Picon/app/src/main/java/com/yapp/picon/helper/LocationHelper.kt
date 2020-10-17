8ef2ccba7361795ab517a2fa72bacb1aebe965ba
kimhyebeen <kimhbin@naver.com>
2020-10-17 17:06:54 +0900

    [Issue-11] 권한이 허용되지 않은 경우, 권한을 요청하는 함수 구현
    앱 실행할 때마다 최신 location을 받아올 때 사용

--- Picon/app/src/main/java/com/yapp/picon/helper/LocationHelper.kt
+++ Picon/app/src/main/java/com/yapp/picon/helper/LocationHelper.kt
@@ -1,9 +1,36 @@
 package com.yapp.picon.helper
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
+import androidx.core.app.ActivityCompat
 
-class LocationHelper(context: Context) {
+class LocationHelper(private val context: Context) {
     private var locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    private var location: Location? = null
 
+    fun requestLocationPermissions(): Location? {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) { // request permissions: 권한이 허용되어 있지 않은 경우 권한 요청
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), RequestCodeSet.LOCATION_REQUEST_CODE.code
+            )
+        } else { // get data
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        }
+        return location
+    }
 }
