abcc8c4a085778c4f3063de09a86acaed607cd9f
NackUn <king4521@naver.com>
2020-10-19 10:28:35 +0900

    [Issue-10] 메인화면 메뉴 ui 작성

--- Picon/app/src/main/java/com/yapp/picon/presentation/map/MapActivity.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/map/MapActivity.kt
@@ -1,6 +1,9 @@
 package com.yapp.picon.presentation.map
 
+import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.core.view.GravityCompat
 import com.naver.maps.map.*
 import com.yapp.picon.R
 import com.yapp.picon.databinding.MapActivityBinding
@@ -16,6 +19,22 @@ class MapActivity : BaseMapActivity<MapActivityBinding, MapViewModel>
 
     private lateinit var map: NaverMap
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setToolBar()
+        setOnClickListeners()
+    }
+
+    private fun setToolBar() {
+        setSupportActionBar(binding.mapToolBar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    private fun setOnClickListeners() {
+        binding.mapBtnMenu.setOnClickListener { binding.mapDrawerLayout.openDrawer(GravityCompat.START) }
+    }
+
     override fun onMapReady(naverMap: NaverMap) {
         this.map = naverMap
         settingOptionToMap()
@@ -29,5 +48,13 @@ class MapActivity : BaseMapActivity<MapActivityBinding, MapViewModel>
             uiSettings.isZoomGesturesEnabled = true
             setLayerGroupEnabled("LAYER_GROUP_BUILDING", true)
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
     }
 }
