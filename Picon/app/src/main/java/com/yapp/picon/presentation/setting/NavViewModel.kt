192d40f326f8a8ef8888d69c76423e41660bfe35
kimhyebeen <kimhbin@naver.com>
2020-10-22 15:08:39 +0900

    [Issue-16] SettingViewModel을 NavViewModel로 이름 변경

--- Picon/app/src/main/java/com/yapp/picon/presentation/setting/NavViewModel.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/setting/NavViewModel.kt
@@ -2,5 +2,5 @@ package com.yapp.picon.presentation.setting
 
 import com.yapp.picon.presentation.base.BaseViewModel
 
-class SettingViewModel: BaseViewModel() {
+class NavViewModel: BaseViewModel() {
 }
