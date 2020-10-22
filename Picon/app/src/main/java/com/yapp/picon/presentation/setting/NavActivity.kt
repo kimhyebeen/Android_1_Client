8bef5a60255102b1021ef9f6d91962ca6d89a3cc
kimhyebeen <kimhbin@naver.com>
2020-10-22 15:26:04 +0900

    [Issue-16] intent 내용에 따라 띄워주는 fragment 설정하도록 구현

--- Picon/app/src/main/java/com/yapp/picon/presentation/setting/NavActivity.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/setting/NavActivity.kt
@@ -1,21 +1,37 @@
 package com.yapp.picon.presentation.setting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import com.yapp.picon.BR
 import com.yapp.picon.R
 import com.yapp.picon.presentation.base.BaseActivity
 import com.yapp.picon.databinding.NavActivityBinding
 
-class SettingActivity: BaseActivity<NavActivityBinding, SettingViewModel>(
+class NavActivity: BaseActivity<NavActivityBinding, NavViewModel>(
     R.layout.nav_activity
 ) {
-    override val vm: SettingViewModel by viewModels()
+    override val vm: NavViewModel by viewModels()
+    private val transaction = supportFragmentManager.beginTransaction()
+    private var fragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initViewModel()
+
+        fragment = intent.getStringExtra("type")
+            ?.let {
+                when(it) {
+                    NavTypeStringSet.Setting.type -> SettingFragment()
+                    else -> null
+                }
+        }
+
+        fragment?.let {
+            transaction.replace(R.id.settingFrame, it).addToBackStack(null).commit()
+        } ?: Log.d("NavActivity", "fragment type이 제대로 전달되지 않았습니다.")
     }
 
     private fun initViewModel() {
---
+++ Picon/app/src/main/java/com/yapp/picon/presentation/setting/NavTypeStringSet.kt
@@ -0,0 +1,9 @@
+package com.yapp.picon.presentation.setting
+
+enum class NavTypeStringSet(val type: String) {
+    CustomEmotion("custom"),
+    ShowPhoto("photo"),
+    ShareMap("share"),
+    ShowGraph("graph"),
+    Setting("setting")
+}
