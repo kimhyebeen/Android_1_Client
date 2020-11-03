dadba619924b50d6e29035c7d4c4a551d357f244
kimhyebeen <kimhbin@naver.com>
2020-11-03 11:19:04 +0900

    [Issue-18] Setting의 세부 기능을 담당할 SettingRepository 생성 및 구현

--- Picon/app/src/main/java/com/yapp/picon/presentation/nav/NavViewModel.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/nav/NavViewModel.kt
@@ -4,10 +4,17 @@ import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.yapp.picon.presentation.base.BaseViewModel
+import com.yapp.picon.presentation.nav.repository.SettingRepository
 
 class NavViewModel: BaseViewModel() {
+    val settingRepository = SettingRepository()
     private val _finishFlag = MutableLiveData<Boolean>()
+
     val finishFlag: LiveData<Boolean> get() = _finishFlag
+    val settingRemoveAllDataFlag: LiveData<Boolean> get() = settingRepository.removeAllDataFlag
+    val settingReviewFlag: LiveData<Boolean> get() = settingRepository.reviewFlag
+    val settingDialogDismissFlag: LiveData<Boolean> get() = settingRepository.dialogDismissFlag
+    val settingDialogRemoveFlag: LiveData<Boolean> get() = settingRepository.dialogRemoveFlag
 
     init {
         _finishFlag.value = false
@@ -17,5 +24,9 @@ class NavViewModel: BaseViewModel() {
         _finishFlag.value = _finishFlag.value?.let {
             !it
         }
+    }
+
+    fun settingInitializeDialogFlag() {
+        settingRepository.initializeSettingFlag()
     }
 }
