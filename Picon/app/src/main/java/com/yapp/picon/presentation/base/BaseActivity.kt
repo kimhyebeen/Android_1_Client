1164f27eeaa6ea605772ede6c103f585cee97565
NackUn <king4521@naver.com>
2020-11-08 19:00:46 +0900

    [Issue-33] 커스텀토스트 추가

--- Picon/app/src/main/java/com/yapp/picon/presentation/base/BaseActivity.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/base/BaseActivity.kt
@@ -1,12 +1,15 @@
 package com.yapp.picon.presentation.base
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
+import com.yapp.picon.R
 
 abstract class BaseActivity<T : ViewDataBinding, VM : ViewModel>(
     @LayoutRes private val layoutId: Int
@@ -26,8 +29,15 @@ abstract class BaseActivity<T : ViewDataBinding, VM : ViewModel>(
         initViewModel()
     }
 
-    protected fun showToast(msg: String) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    protected open fun showToast(msg: String) {
+        val customToast = layoutInflater.inflate(R.layout.custom_toast, findViewById(R.id.custom_toast))
+        customToast.findViewById<TextView>(R.id.custom_toast_tv).text = msg
+        Toast(this).run {
+            setGravity(Gravity.BOTTOM, 0, 200)
+            duration = Toast.LENGTH_SHORT
+            view = customToast
+            show()
+        }
     }
 
 }
