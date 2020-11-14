4edabf386a4fe8793540f6d7793e8c278022f19a
NackUn <king4521@naver.com>
2020-11-14 23:56:35 +0900

    [Issue-37] DataStore DataSource 추가

--- Picon/app/src/main/java/com/yapp/picon/data/di/LocalModule.kt
+++ Picon/app/src/main/java/com/yapp/picon/data/di/LocalModule.kt
@@ -1,11 +1,18 @@
 package com.yapp.picon.data.di
 
+import androidx.datastore.preferences.createDataStore
+import com.yapp.picon.data.source.searched.DataStoreDataSource
 import com.yapp.picon.data.source.searched.SearchedDataSource
 import com.yapp.picon.data.source.searched.local.SearchedDatabase
 import com.yapp.picon.data.source.searched.local.SearchedLocalDataSource
+import com.yapp.picon.data.source.searched.remote.DataStoreLocalDataSource
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val localModule = module {
     single { get<SearchedDatabase>().searchedDao() }
     single<SearchedDataSource> { SearchedLocalDataSource(get()) }
+
+    single { androidContext().createDataStore("data_store") }
+    single<DataStoreDataSource> { DataStoreLocalDataSource(get()) }
 }
---
+++ Picon/app/src/main/java/com/yapp/picon/data/source/datastore/local/DataStoreLocalDataSource.kt
@@ -0,0 +1,28 @@
+package com.yapp.picon.data.source.searched.remote
+
+import androidx.datastore.DataStore
+import androidx.datastore.preferences.Preferences
+import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.preferencesKey
+import com.yapp.picon.data.source.searched.DataStoreDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class DataStoreLocalDataSource(
+    private val dataStore: DataStore<Preferences>
+) : DataStoreDataSource {
+
+    private val accessToken = preferencesKey<String>("ACCESS_TOKEN")
+
+    override suspend fun saveAccessToken(accessToken: String) {
+        dataStore.edit { preferences ->
+            preferences[this.accessToken] = accessToken
+        }
+    }
+
+    override suspend fun loadAccessToken(): Flow<String> =
+        dataStore.data
+            .map { preferences ->
+                preferences[this.accessToken] ?: ""
+            }
+}
