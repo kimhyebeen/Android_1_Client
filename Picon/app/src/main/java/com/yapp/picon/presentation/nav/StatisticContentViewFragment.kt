cd0dccac4caa2c8ecb7110de0b4e22112a797498
kimhyebeen <kimhbin@naver.com>
2020-11-20 02:40:02 +0900

    [Issue-27] 감정별 그래프 adapter 구현 및 적용

--- Picon/app/src/main/java/com/yapp/picon/presentation/nav/StatisticContentViewFragment.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/nav/StatisticContentViewFragment.kt
@@ -1,5 +1,6 @@
 package com.yapp.picon.presentation.nav
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -8,12 +9,14 @@ import com.yapp.picon.BR
 import com.yapp.picon.R
 import com.yapp.picon.databinding.NavStatisticContentViewBinding
 import com.yapp.picon.presentation.base.BaseFragment
+import com.yapp.picon.presentation.nav.adapter.EmotionGraphAdapter
 import com.yapp.picon.presentation.nav.adapter.PlaceGraphAdapter
 
 class StatisticContentViewFragment: BaseFragment<NavStatisticContentViewBinding, NavViewModel>(
     R.layout.nav_statistic_content_view
 ) {
     private lateinit var placeAdapter: PlaceGraphAdapter
+    private lateinit var emotionAdapter: EmotionGraphAdapter
 
     @Suppress("UNCHECKED_CAST")
     override val vm: NavViewModel by activityViewModels {
@@ -29,18 +32,58 @@ class StatisticContentViewFragment: BaseFragment<NavStatisticContentViewBinding,
     override fun onStart() {
         super.onStart()
 
+        setEmotionAdapter()
+        setPlaceAdapter()
+        observeGraphData()
+    }
+
+    @SuppressLint("ResourceType")
+    private fun setEmotionAdapter() {
+        emotionAdapter = EmotionGraphAdapter(
+            listOf(
+                getString(R.color.soft_blue),
+                getString(R.color.cornflower),
+                getString(R.color.bluegrey),
+                getString(R.color.very_light_brown),
+                getString(R.color.warm_grey)
+            ),
+            R.layout.emotion_graph_item,
+            BR.emotionItem
+        )
+        binding.navStatisticEmotionGraphRv.apply {
+            adapter = emotionAdapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun setPlaceAdapter() {
         placeAdapter = PlaceGraphAdapter(R.layout.place_graph_item, BR.item)
         binding.navStatisticPlaceGraphRv.apply {
             adapter = placeAdapter
             layoutManager = LinearLayoutManager(context)
             setHasFixedSize(true)
         }
-        observeGraphData()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun observeGraphData() {
         vm.statisticRepository.placeList.observe(this, {
             placeAdapter.setItems(it)
+        })
+        vm.statisticRepository.emotionList.observe(this, {
+            emotionAdapter.setItems(it)
+            var max = 0
+            it.map { item ->
+                if (max < item.count) max = item.count
+            }
+            emotionAdapter.setMaxCount(max)
+
+            var sum = 0
+            it.map { item ->
+                sum += item.count
+            }
+            binding.navStatisticPinNumberTv.text = "$sum 핀"
         })
     }
 }
--- Picon/app/src/main/java/com/yapp/picon/presentation/nav/repository/StatisticRepository.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/nav/repository/StatisticRepository.kt
@@ -3,6 +3,7 @@ package com.yapp.picon.presentation.nav.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.broooapps.lineargraphview2.DataModel
+import com.yapp.picon.presentation.model.StatisticEmotionGraphItem
 import com.yapp.picon.presentation.model.StatisticPlaceGraphItem
 
 class StatisticRepository {
@@ -10,11 +11,14 @@ class StatisticRepository {
     private val _month = MutableLiveData<Int>()
     private val _title = MutableLiveData<String>()
     private val _placeList = MutableLiveData<List<StatisticPlaceGraphItem>>()
+    private val _emotionList = MutableLiveData<List<StatisticEmotionGraphItem>>()
 
     init {
         _year.value = 2020
         _month.value = 11
         _title.value = "11월 여행 통계"
+
+        // todo - placeList는 api에서 받아와요.
         _placeList.value = listOf(
             StatisticPlaceGraphItem("서울 관악구", listOf(
                 DataModel("감정1", "#8187da", 10),
@@ -35,14 +39,36 @@ class StatisticRepository {
                 DataModel("감정5", "#9a948b", 10)
             ), 110)
         )
+
+        // todo - emotionList는 api에서 받아와요.
+        _emotionList.value = listOf(
+            StatisticEmotionGraphItem("새벽 3시", 21),
+            StatisticEmotionGraphItem("구름없는 하늘", 11),
+            StatisticEmotionGraphItem("아침 이슬", 5),
+            StatisticEmotionGraphItem("창문 너머 노을", 4),
+            StatisticEmotionGraphItem("잔잔한 밤", 1)
+        )
     }
 
     val year: LiveData<Int> get() = _year
     val month: LiveData<Int> get() = _month
     val title: LiveData<String> get() = _title
     val placeList: LiveData<List<StatisticPlaceGraphItem>> get() = _placeList
+    val emotionList: LiveData<List<StatisticEmotionGraphItem>> get() = _emotionList
 
     fun setPlaceList(list: List<StatisticPlaceGraphItem>) {
         _placeList.value = list
+    }
+
+    fun setYear(value: Int) {
+        _year.value = value
+    }
+
+    fun setMonth(value: Int) {
+        _month.value = value
+    }
+
+    fun setTitle(value: String) {
+        _title.value = value
     }
 }
