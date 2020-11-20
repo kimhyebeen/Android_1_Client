5b4f5cb98c7692807a3a4018626777543b2640d4
NackUn <king4521@naver.com>
2020-11-20 13:57:17 +0900

    [PR-39] NaverApi Key 이동

--- Picon/app/src/main/java/com/yapp/picon/data/api/NaverApi.kt
+++ Picon/app/src/main/java/com/yapp/picon/data/api/NaverApi.kt
@@ -1,5 +1,6 @@
 package com.yapp.picon.data.api
 
+import com.yapp.picon.SecretKeySet
 import com.yapp.picon.data.model.LocalResult
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -7,8 +8,8 @@ import retrofit2.http.Query
 
 interface NaverApi {
     @Headers(
-        "X-Naver-Client-Id: MB0QmA3dWk5pHT2Mbz7_",
-        "X-Naver-Client-Secret: jgCRJfYDDT"
+        "X-Naver-Client-Id: ${SecretKeySet.X_Naver_Client_Id}",
+        "X-Naver-Client-Secret: ${SecretKeySet.X_Naver_Client_Secret}"
     )
     @GET("v1/search/local.json")
     suspend fun requestLocal(
