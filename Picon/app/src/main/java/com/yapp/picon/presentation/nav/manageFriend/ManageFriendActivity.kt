7146a5da4e02d9a839a52ad7ec983e44589ee8d4
kimhyebeen <kimhbin@naver.com>
2020-12-03 01:32:11 +0900

    [Issue-72] 친구관리 - 검색창 X버튼 활성화

--- Picon/app/src/main/java/com/yapp/picon/presentation/nav/manageFriend/ManageFriendActivity.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/nav/manageFriend/ManageFriendActivity.kt
@@ -1,6 +1,9 @@
 package com.yapp.picon.presentation.nav.manageFriend
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.yapp.picon.BR
 import com.yapp.picon.R
 import com.yapp.picon.databinding.ManageFriendActivityBinding
@@ -16,11 +19,26 @@ class ManageFriendActivity : BaseActivity<ManageFriendActivityBinding, ManageFri
         vm.backButton.observe(this, {
             if (it) onBackPressed()
         })
+        vm.searchText.observe(this, {
+            if (it.isEmpty()) {
+                binding.manageFriendSearchDeleteButton.visibility = View.GONE
+                onHideKeypad()
+            } else {
+                binding.manageFriendSearchDeleteButton.visibility = View.VISIBLE
+            }
+        })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.setVariable(BR.manageVM, vm)
+    }
+
+    private fun onHideKeypad() {
+        val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        val view: View = currentFocus ?: View(this)
+        view.clearFocus()
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
--- Picon/app/src/main/java/com/yapp/picon/presentation/nav/manageFriend/ManageFriendViewModel.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/nav/manageFriend/ManageFriendViewModel.kt
@@ -9,13 +9,20 @@ class ManageFriendViewModel: BaseViewModel() {
     private val _backButton = MutableLiveData<Boolean>()
     val backButton: LiveData<Boolean> get() =  _backButton
 
+    val searchText = MutableLiveData<String>()
+
     init {
         _backButton.value = false
+        searchText.value = ""
     }
 
     fun clickBackButton(view: View) {
         _backButton.value?.let {
             _backButton.value = !it
         }
+    }
+
+    fun clickSearchDeleteButton(view: View) {
+        searchText.value = ""
     }
 }
