aeeec182944423a65070c250e102da59035853bd
kimhyebeen <kimhbin@naver.com>
2020-12-12 23:42:35 +0900

    [Issue-81] 게시글편집 - 뒤로가기 버튼 활성화 및 dialog 구현

--- Picon/app/src/main/java/com/yapp/picon/presentation/post/EditPostActivity.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/post/EditPostActivity.kt
@@ -1,7 +1,16 @@
 package com.yapp.picon.presentation.post
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.res.Resources
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
 import com.yapp.picon.BR
 import com.yapp.picon.R
+import com.yapp.picon.databinding.DialogEditPostFinishBinding
 import com.yapp.picon.databinding.EditPostActivityBinding
 import com.yapp.picon.presentation.base.BaseActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -9,6 +18,9 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class EditPostActivity: BaseActivity<EditPostActivityBinding, EditPostViewModel>(
     R.layout.edit_post_activity
 ) {
+    private lateinit var finishDialog: Dialog
+    private lateinit var finishBuilder: AlertDialog.Builder
+
     override val vm: EditPostViewModel by viewModel()
 
     override fun initViewModel() {
@@ -16,14 +28,45 @@ class EditPostActivity: BaseActivity<EditPostActivityBinding, EditPostViewModel>
 
         vm.backButton.observe(this) {
             if (it) {
-                // todo - dialog 띄우기
+                setFinishDialog()
+                finishDialog.show()
+                setDialogSize(finishDialog)
+            }
+        }
+
+        vm.finishDialogConfirmButton.observe(this) {
+            if (it) finish()
+        }
+
+        vm.finishDialogCancelButton.observe(this) {
+            if (it) {
+                finishDialog.dismiss()
+                vm.initialize()
             }
         }
     }
 
-    override fun onResume() {
-        super.onResume()
+    private fun setFinishDialog() {
+        val dialogFinishView: DialogEditPostFinishBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(this), R.layout.dialog_edit_post_finish, null, false
+        )
+        dialogFinishView.setVariable(BR.depVM, vm)
+        finishBuilder = AlertDialog.Builder(this)
+        finishBuilder.setView(dialogFinishView.root)
 
-        vm.initialize()
+        finishDialog = finishBuilder.create()
+        finishDialog.setCancelable(false)
+        finishDialog.window?.apply {
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+    }
+
+    private fun setDialogSize(dialog: Dialog) {
+        val layoutParams = WindowManager.LayoutParams().apply {
+            copyFrom(dialog.window!!.attributes)
+            width = (274 * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+            height = WindowManager.LayoutParams.WRAP_CONTENT
+        }
+        dialog.window?.attributes = layoutParams
     }
 }
--- Picon/app/src/main/java/com/yapp/picon/presentation/post/EditPostViewModel.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/post/EditPostViewModel.kt
@@ -9,17 +9,37 @@ class EditPostViewModel: BaseViewModel() {
     private val _backButton = MutableLiveData<Boolean>()
     val backButton: LiveData<Boolean> get() =  _backButton
 
+    private val _finishDialogConfirmButton = MutableLiveData<Boolean>()
+    val finishDialogConfirmButton: LiveData<Boolean> get() =  _finishDialogConfirmButton
+
+    private val _finishDialogCancelButton = MutableLiveData<Boolean>()
+    val finishDialogCancelButton: LiveData<Boolean> get() =  _finishDialogCancelButton
+
     init {
         initialize()
     }
 
     fun initialize() {
         _backButton.value = false
+        _finishDialogConfirmButton.value = false
+        _finishDialogCancelButton.value = false
     }
 
     fun clickBackButton(view: View) {
         _backButton.value?.let {
             _backButton.value = !it
+        }
+    }
+
+    fun clickFinishDialogConfirmButton(view: View) {
+        _finishDialogConfirmButton.value?.let {
+            _finishDialogConfirmButton.value = !it
+        }
+    }
+
+    fun clickFinishDialogCancelButton(view: View) {
+        _finishDialogCancelButton.value?.let {
+            _finishDialogCancelButton.value = !it
         }
     }
 }
