bc42029032b2a304ef681023df0a2d5a7b9308e4
kimhyebeen <kimhbin@naver.com>
2020-12-12 10:43:12 +0900

    [Issue-81] 게시글편집 - View와 ViewModel 연결

--- Picon/app/src/main/java/com/yapp/picon/presentation/post/EditPostActivity.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/post/EditPostActivity.kt
@@ -1,5 +1,6 @@
 package com.yapp.picon.presentation.post
 
+import com.yapp.picon.BR
 import com.yapp.picon.R
 import com.yapp.picon.databinding.EditPostActivityBinding
 import com.yapp.picon.presentation.base.BaseActivity
@@ -11,5 +12,18 @@ class EditPostActivity: BaseActivity<EditPostActivityBinding, EditPostViewModel>
     override val vm: EditPostViewModel by viewModel()
 
     override fun initViewModel() {
+        binding.setVariable(BR.epVM, vm)
+
+        vm.backButton.observe(this) {
+            if (it) {
+                // todo - dialog 띄우기
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        vm.initialize()
     }
 }
--- Picon/app/src/main/java/com/yapp/picon/presentation/post/EditPostViewModel.kt
+++ Picon/app/src/main/java/com/yapp/picon/presentation/post/EditPostViewModel.kt
@@ -1,6 +1,25 @@
 package com.yapp.picon.presentation.post
 
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.yapp.picon.presentation.base.BaseViewModel
 
 class EditPostViewModel: BaseViewModel() {
+    private val _backButton = MutableLiveData<Boolean>()
+    val backButton: LiveData<Boolean> get() =  _backButton
+
+    init {
+        initialize()
+    }
+
+    fun initialize() {
+        _backButton.value = false
+    }
+
+    fun clickBackButton(view: View) {
+        _backButton.value?.let {
+            _backButton.value = !it
+        }
+    }
 }
